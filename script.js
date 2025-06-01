353e56445764eeba0443bf42ff16b3dda123dd80
Gabriel Cortes <progamingfullstackGC@outlook.com>
2025-05-31 23:27:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 /* Desenvolva seu código abaixo */
+
+function playRockPaperScissor(player1, player2) {
+  if (player1 === player2) {
+    return "Empate!";
+  }
+
+  if (
+    (player1 === "Pedra" && player2 === "Tesoura") ||
+    (player1 === "Tesoura" && player2 === "Papel") ||
+    (player1 === "Papel" && player2 === "Pedra")
+  ) {
+    return "Jogador 1 venceu!";
+  } else {
+    return "Jogador 2 venceu!";
+  }
+}
+
+console.log(playRockPaperScissor("Pedra", "Tesoura")); 
+console.log(playRockPaperScissor("Papel", "Tesoura")); 
+console.log(playRockPaperScissor("Tesoura", "Tesoura")); 
+
+
