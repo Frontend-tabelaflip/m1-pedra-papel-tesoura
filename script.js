068f7fe45e4d156afa447a18c6f3fa9b0f3e2ef0
Gabriel Cortes <progamingfullstackGC@outlook.com>
2025-05-31 23:12:53 -0300

    Update script.js
    meu  codigo, aluno kenzie

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+function playRockPaperScissor (player1, player2){
+  if( player1 === player2){
+    return "empate"
+  }
+ alese if(
+   (player1 === "pedra" && palyer2 ==="tesoura") || 
+   (player1 === "tesoura" && player2 ==== "papel") ||
+   (player1 === "papel" && player === "pedra")
+ ){
+   return "player 1 ganhou"
+ }
+ ruturn "player2 ganhou"
+} 
+console.log(pedra, tesoura)
+console.log(tesoura, papel)
+console.log(papel, pedra)
 /* Desenvolva seu código abaixo */
